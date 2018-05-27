8851b5e034740200eefc8122520fb1b2f5513843
UGA <s.ugagod@gmail.com>
2018-05-27 18:50:27 +0900

    Initial Commit

---
+++ DrawBooking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawBooking
+//
+//  Created by Seiya Ugajin on 2018/05/27.
+//  Copyright © 2018年 Seiya Ugajin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
